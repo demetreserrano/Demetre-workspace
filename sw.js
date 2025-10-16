8c4c0d330af5fd19c9e2e60f264d9e3687e541d5
demetreserrano <demetreserrano@gmail.com>
2025-10-16 07:21:09 -0500

    Add files via upload
    Ux fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'coach-v3';
+const CACHE = 'coach-v3-1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(self.skipWaiting()));
